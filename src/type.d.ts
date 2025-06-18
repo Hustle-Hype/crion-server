78228f3e17d70f5a541c0d45c49c3e1e3244f54c
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-06-18 12:40:34 +0700

    Update User interface in type definitions to include optional fields and additional properties
    - Modified the User interface in type.d.ts to make existing fields optional and added new properties such as id, provider, emails, displayName, username, profileUrl, and photos for enhanced user profile management. This improves flexibility and aligns with evolving user data requirements. #5

--- src/type.d.ts
+++ src/type.d.ts
@@ -5,11 +5,18 @@ import { IAccount } from './models/schemas/account.schema'
 declare global {
   namespace Express {
     interface User {
-      issuer: IIssuer
-      account: IAccount
-      accessToken: string
-      refreshToken: string
-      _type: 'authenticated_session'
+      issuer?: IIssuer
+      account?: IAccount
+      accessToken?: string
+      refreshToken?: string
+      _type?: 'authenticated_session'
+      id: string
+      provider: string
+      emails?: Array<{ value: string }>
+      displayName?: string
+      username?: string
+      profileUrl?: string
+      photos?: Array<{ value: string }>
     }
 
     interface Request {
